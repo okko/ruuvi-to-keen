70af45c0e7a7908eaaece4a4b972c33b5d88aa82
Oskari Okko Ojala <git@okko.fi>
2017-08-26 22:33:57 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const Keen = require('keen-js');
+const ruuvi = require('node-ruuvitag');
+
+const keenClient = new Keen({
+  projectId: process.env.KEEN_PROJECTID,
+  writeKey: process.env.KEEN_WRITEKEY
+});
+
+ruuvi.on('found', tag => {
+  console.log(tag.id + ' found');
+  tag.on('updated', data => {
+    console.log(tag.id + ' data received: ' + JSON.stringify(data, null, ''));
+    keenClient.recordEvent(
+      'measurement',
+      {
+        sensor: tag.id,
+        rssi: data.rssi,
+        humidity: data.humidity,
+        temperature: data.temperature,
+        pressure: data.pressure
+      },
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
+});
